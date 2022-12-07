3566469cc7e1fda8c970b912c736dff95ef025ff
Darya Naji <daaryaa.naji@gmail.com>
2022-12-07 23:10:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const p = document.createElement("p");
+const newHeader = document.createElement("h1");
+
+document.getElementById("main").remove();
+console.log("first")
+p.style.color = "red";
+//newHeader.id = "victory";
+newHeader.setAttribute("id", "victory")
+
+newHeader.innerHTML = "is the champion"
+document.body.append(newHeader);
+document.body.append(p);
+p.innerHTML = "hello world"
